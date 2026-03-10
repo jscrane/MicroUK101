369e2d833e21f69c7052b0795e7bbb8d06026e32
steve <jscrane@gmail.com>
2026-03-10 10:42:53 +0000

    ACIA is now a Memory::Device again

--- MicroUK101.ino
+++ MicroUK101.ino
@@ -15,76 +15,65 @@
 Memory memory;
 r6502 cpu(memory);
 Arduino machine(cpu);
+ACIA acia;
 
 static uint32_t acia_framing(uint8_t b) {
-  switch (b) {
-    case ACIA::ws7e2:
-		  return SERIAL_7E2;
-    case ACIA::ws7o2:
-		  return SERIAL_7O2;
-    case ACIA::ws7e1:
-		  return SERIAL_7E1;
-    case ACIA::ws7o1:
-		  return SERIAL_7O1;
-    case ACIA::ws8n2:
-		  return SERIAL_8N2;
-    case ACIA::ws8n1:
-		  return SERIAL_8N1;
-    case ACIA::ws8e1:
-		  return SERIAL_8E1;
-    case ACIA::ws8o1:
-		  return SERIAL_8O1;
+	switch (b) {
+	case ACIA::ws7e2:
+		return SERIAL_7E2;
+	case ACIA::ws7o2:
+		return SERIAL_7O2;
+	case ACIA::ws7e1:
+		return SERIAL_7E1;
+	case ACIA::ws7o1:
+		return SERIAL_7O1;
+	case ACIA::ws8n2:
+		return SERIAL_8N2;
+	case ACIA::ws8n1:
+		return SERIAL_8N1;
+	case ACIA::ws8e1:
+		return SERIAL_8E1;
+	case ACIA::ws8o1:
+		return SERIAL_8O1;
 	}
-  return SERIAL_8N1;
+	return SERIAL_8N1;
 }
 
-class SerialAcia: public Memory::Device {
-public:
-	SerialAcia(): Memory::Device(2048) {}
-
-	void init() {
-		_acia.register_framing_handler([](uint8_t b) {
-      uint32_t cfg = acia_framing(b);
+static void acia_init() {
+	acia.register_framing_handler([](uint8_t b) {
+		uint32_t cfg = acia_framing(b);
 #if DEBUGGING == DEBUG_NONE
-      Serial.begin(TERMINAL_SPEED, cfg);
+		Serial.begin(TERMINAL_SPEED, cfg);
 #endif
-      DBG_EMU("framing: %x\r\n", cfg);
-		});
-		_acia.register_read_data_handler([]() {
-			uint8_t b = Serial.read();
-			DBG_EMU("read: %02x", b);
-			if (b == 0x0e)		// ^N
-				machine.reset();
-			else if (b == 0x08)	// BS
-				b = '_';
-			return b;
-		});
-		_acia.register_write_data_handler([](uint8_t b) {
-			DBG_EMU("write: %02x", b);
-			if (b == '_') {
-				Serial.write(0x08);
-				Serial.write(' ');
-				Serial.write(0x08);
-				return;
-			}
-			Serial.write(b);
-		});
-		_acia.register_can_rw_handler([](void) {
-			uint8_t s = 0;
-			if (Serial.available() > 0) s++;
-			if (Serial.availableForWrite() > 0) s += 2;
-			DBG_EMU("can_rw: %x", s);
-			return s;
-		});
-	}
-
-	virtual void operator=(uint8_t b) { _acia.write(_acc, b); }
-	virtual operator uint8_t() { return _acia.read(_acc); }
-
-private:
-	ACIA _acia;
-
-} acia;
+		DBG_EMU("framing: %x\r\n", cfg);
+	});
+	acia.register_read_data_handler([]() {
+		uint8_t b = Serial.read();
+		DBG_EMU("read: %02x", b);
+		if (b == 0x0e)		// ^N
+			machine.reset();
+		else if (b == 0x08)	// BS
+			b = '_';
+		return b;
+	});
+	acia.register_write_data_handler([](uint8_t b) {
+		DBG_EMU("write: %02x", b);
+		if (b == '_') {
+			Serial.write(0x08);
+			Serial.write(' ');
+			Serial.write(0x08);
+			return;
+		}
+		Serial.write(b);
+	});
+	acia.register_can_rw_handler([](void) {
+		uint8_t s = 0;
+		if (Serial.available() > 0) s++;
+		if (Serial.availableForWrite() > 0) s += 2;
+		DBG_EMU("can_rw: %x", s);
+		return s;
+	});
+}
 
 prom tk2(toolkit2, 2048);
 prom enc(encoder, 2048);
@@ -100,32 +89,32 @@ ram<> pages[32];
 
 void setup() {
 
-  machine.begin();
+	machine.begin();
 
-  for (unsigned i = 0; i < 32; i++)
-    memory.put(pages[i], i * ram<>::page_size);
+	for (unsigned i = 0; i < 32; i++)
+		memory.put(pages[i], i * ram<>::page_size);
 
-  memory.put(tk2, 0x8000);
-  memory.put(enc, 0x8800);
-  memory.put(basic5, 0x9000);
-  memory.put(basic6, 0x9800);
-  memory.put(basic1, 0xa000);
-  memory.put(basic2, 0xa800);
-  memory.put(basic3, 0xb000);
-  memory.put(basic4, 0xb800);
+	memory.put(tk2, 0x8000);
+	memory.put(enc, 0x8800);
+	memory.put(basic5, 0x9000);
+	memory.put(basic6, 0x9800);
+	memory.put(basic1, 0xa000);
+	memory.put(basic2, 0xa800);
+	memory.put(basic3, 0xb000);
+	memory.put(basic4, 0xb800);
 
-  memory.put(acia, 0xf000);
-  memory.put(cegmon, 0xf800);
+	memory.put(acia, 0xf000, 2048);
+	memory.put(cegmon, 0xf800);
 
-	acia.init();
+	acia_init();
 
-  /* debugging
-  machine.register_cpu_debug_handler([]() {
-   return cpu.pc() >= 0x1000 && cpu.pc() < 0x2000;
-  });
-  */
+	/* debugging
+	machine.register_cpu_debug_handler([]() {
+	 return cpu.pc() >= 0x1000 && cpu.pc() < 0x2000;
+	});
+	*/
 
-  machine.reset();
+	machine.reset();
 }
 
 void loop() {
